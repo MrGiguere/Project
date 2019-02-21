107828f397df1eea1c41539bdb98023ad8a98634
HoangLongAllen <45009291+HoangLongAllen@users.noreply.github.com>
2019-02-21 12:08:22 -0500

    Update BTC marketplace.py

--- BTC marketplace.py
+++ BTC marketplace.py
@@ -1,11 +1,3 @@
-##File: project2.py
-##Author: Ethan Pafford, Max Babka, Eric Zhang, Allen Ngo
-##Date: Due 3/4/2019
-##Class: AP Computer Science Principles period 3
-##Teacher: Mr. Giguere
-##Code Description: 
-
-
 #Create different wallets for different users
 #Create a mine function that gives a set amount of BTC
 #Transfer bitcoin between wallets
@@ -13,29 +5,25 @@
 
 
 class Wallet: #Creates the Wallet class
-    def __init__(self,username,password, pin):
-        self.username = username
-        self.password = password
-        self.pin = pin
+    def __init__(self,username,password, pin, balance):
+    self.username = username
+    self.password = password
+    self.pin = pin
+    self.balance = balance
 
 
 
 def deleteWallet(name, password): #deletes a user's wallet
     if name.password == password:
         del name
-        print("Deleted.") #correct password, Deleted!
+        print("Deleted.")
     else:
-        print("Error wrong password.") #incorrect password
+        print("Error wrong password.")
 
-
-    
-
-
-
-
-
-
-
+def transferWallet(name, password, amount, address):
+    if name.password == password:
+        self.balance = self.balance - amount
+        address.balance = address.balance + amount
 
 
 
