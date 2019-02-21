4e58194a6b33340737e8df6c45e7371eaa545f53
quantumblurinc <31797536+quantumblurinc@users.noreply.github.com>
2019-02-21 12:06:18 -0500

    Update BTC marketplace.py

--- BTC marketplace.py
+++ BTC marketplace.py
@@ -1,3 +1,11 @@
+
+
+
+
+
+
+
+
 #Create different wallets for different users
 #Create a mine function that gives a set amount of BTC
 #Transfer bitcoin between wallets
@@ -6,9 +14,9 @@
 
 class Wallet: #Creates the Wallet class
     def __init__(self,username,password, pin):
-    self.username = username
-    self.password = password
-    self.pin = pin
+        self.username = username
+        self.password = password
+        self.pin = pin
 
 
 
