33f34a4ee3ca9d2254d40febd56c25c5bba3372e
Max Babka <39202268+MaxBabka@users.noreply.github.com>
2019-03-03 12:13:03 -0500

    Update BTC marketplace.py

--- BTC marketplace.py
+++ BTC marketplace.py
@@ -6,46 +6,31 @@
 import random
 
 class Wallet: #Creates the Wallet class
-    def __init__(self,username,password, pin, balance):
-    self.username = username
-    self.password = password
-    self.pin = pin
-    self.balance = balance
+    def __init__(self, password, pin, balance):
+        self.password = password
+        self.pin = pin
+        self.balance = balance
 
-
-
-def deleteWallet(name, password): #deletes a user's wallet
-    if name.password == password:
-        del name
-        print("Deleted.")
-    else:
-        print("Error wrong password.")
-
-def transferWallet(name, password, amount, address):
-    if name.password == password:
-        self.balance = self.balance - amount
-        address.balance = address.balance + amount
-
-def mineBTC(import('guess a number between 0 & 5')
-    number = random.randint(0,6)
-    if import() = number
-            
+    def checkBalance(self):
+        print(self.balance)
     
+    def deleteWallet(self, name, password): #deletes a user's wallet
+        if name.password == password:
+            del name
+            print("Deleted.")
+        else:
+            print("Error wrong password.")
 
+    def transferBitcoin(self,password, amount, address):
+        if self.password == password:
+            self.balance = self.balance - amount
+            address.balance = address.balance + amount
+        else:
+            print("Error!")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def mineBitcoin(self, guess):
+        number = random.randint(0,6)
+        if guess == number:
+            balance = balance+1
+            print("Your balance has been updated, your wallet contains: "+str(self.balance))
 
